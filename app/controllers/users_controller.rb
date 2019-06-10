972be0f1e133eb08adac03d0829163dd7aa57781
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 03:01:34 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
