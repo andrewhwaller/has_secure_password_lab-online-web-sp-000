c2d84ae618a94c46e342ac1a2e754fde0a482492
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 02:50:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
 
   post '/login' => 'sessions#create'
 
-  get 'sessions/destroy'
+  get '/logout' => 'sessions#destroy'
 
-  get 'users/new'
-
-  get 'users/create'
-
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
